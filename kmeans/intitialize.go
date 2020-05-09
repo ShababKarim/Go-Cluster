522fc642c8ea7bb993e8f61f443a2f7f6fd2e519
Shabab Karim <skarim222@gmail.com>
2020-05-09 00:30:48 -0500

    organized kmeans steps and fixied initial assignment

--- kmeans/intitialize.go
+++ kmeans/intitialize.go
@@ -6,27 +6,26 @@ import (
 )
 
 // InitialAssign randomly chooses first elem of each cluster
-func InitialAssign(rawObs [][]int, count int) (map[int][][]int, [][]int, error){
+func InitialAssign(obs [][]int, count int) (map[int][][]int, error){
 	var err error
 
-	if len(rawObs) == 0 {
+	if len(obs) == 0 {
 		err = errors.New("No observations")
-	} else if len(rawObs) < count || count < 1 {
+	} else if len(obs) < count || count < 1 {
 		err = errors.New("Invalid cluster number")
 	}
 	
-	obs := make([][]int, len(rawObs))
-	copy(obs, rawObs)
 	clusters := make(map[int][][]int)
 
-	for i := 0; i < count; i++ {
-		// select obs
-		index := rand.Intn(len(obs))
-		// create a list at cluster
-		clusterObs := append(make([][]int, 0), obs[index])
-		clusters[i] = clusterObs
-		// remove from obs slice
-		obs = append(obs[0:index], obs[index+1:]...)
+	for _, vector := range obs {
+		// select random cluster number
+		k := rand.Intn(count)
+		// assign observation to cluster
+		if _, exists := clusters[k]; exists {
+			clusters[k] = append(clusters[k], vector)
+		} else {
+			clusters[k] = append(make([][]int, 0), vector)
+		}
 	}
-	return clusters, obs, err
+	return clusters, err
 }
--- kmeans/update/update.go
+++ kmeans/update/update.go
@@ -1,13 +1,18 @@
 package kmeans
 
 // Update iterates until clusters stop changing
-func Update(clusters map[int][][]int, obs[][]int) {
+func Update(clusters map[int][][]int, obs[][]int, numFeatures int) (map[int][][]int, error){
+	var err error
+
 	isChanged := true 
 	for isChanged {
 		// compute centroid of each cluster
+		centroids, err := CentroidList(clusters, numFeatures)
 		// identify nearest cluster
 		// assign observation to nearest cluster
 		// a. check if exists already
 		// b. append to cluster
 	}
+
+	return clusters, err
 }

8b0856ab97758e7c4d513dd728d5a238964be4f2
Shabab Karim <skarim222@gmail.com>
2020-05-12 18:18:46 -0500

    test centroid list

--- kmeans/algorithm/iteration_test.go
+++ kmeans/algorithm/iteration_test.go
@@ -53,3 +53,52 @@ func testPerfectMatch() func(t *testing.T) {
 		}
 	}
 }
+
+func TestCentroidList(t *testing.T) {
+	t.Run("less features than in map", testIncorrectCentroid())
+	t.Run("correct case", testCorrectCentroid())
+}
+
+func testIncorrectCentroid() func(t *testing.T) {
+	// not handling this case
+	// err should not be nil
+	clusterMap := make(map[int][][]int)
+	clusterMap[0] = make([][]int, 1)
+	clusterMap[1] = make([][]int, 1)
+
+	clusterMap[0][0] = []int{ 1, 2, 3 }
+	clusterMap[1][0] = []int{ 2, 4 }
+	numFeatures := 2
+
+	return func(t *testing.T) {
+		_, err := CentroidList(clusterMap, numFeatures)
+		if err != nil {
+			t.Error("Expected error to be nil")
+		}
+	}
+}
+
+func testCorrectCentroid() func(t *testing.T) {
+	clusterMap := make(map[int][][]int)
+	clusterMap[0] = make([][]int, 2)
+	clusterMap[1] = make([][]int, 2)
+
+	clusterMap[0][0] = []int{ 1, 4 }
+	clusterMap[0][1] = []int{ 3, 6 }
+
+	clusterMap[1][0] = []int{ 1, 4 }
+	clusterMap[1][1] = []int{ 3, 6 }
+	numFeatures := 2
+	return func(t *testing.T) {
+		centroids, err := CentroidList(clusterMap, numFeatures)
+		if err != nil {
+			t.Error("Expected error to be nil")
+		}
+
+		for i := 0; i < numFeatures; i++ {
+			if centroids[0][i] != centroids[1][i] {
+				t.Error("Incorrect centroid value")
+			}
+		}
+	}
+}
